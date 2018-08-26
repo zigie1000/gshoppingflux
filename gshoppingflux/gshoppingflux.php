fe8e1c31baaf15b5895ba8ac76478d8e670bc375
Casper Olsen <privat@linkcube.dk>
2018-08-26 19:27:33 +0200

    Fix NOTICE
    fix lots of NOTICE

--- gshoppingflux/gshoppingflux.php
+++ gshoppingflux/gshoppingflux.php
@@ -2643,35 +2643,39 @@ class GShoppingFlux extends Module
     public function linkencode($p_url)
     {
         $ta = parse_url($p_url);
-        if (!empty($ta[scheme])) {
-            $ta[scheme].='://';
+        if (!empty($ta['scheme'])) {
+            $ta['scheme'].='://';
         }
-        if (!empty($ta[pass]) and !empty($ta[user])) {
-            $ta[user].=':';
-            $ta[pass]=rawurlencode($ta[pass]).'@';
-        } elseif (!empty($ta[user])) {
-            $ta[user].='@';
+        if (!empty($ta['pass']) and !empty($ta['user'])) {
+            $ta['user'].=':';
+            $ta['pass']=rawurlencode($ta['pass']).'@';
+        } elseif (!empty($ta['user'])) {
+            $ta['user'].='@';
+        } else {
+            $ta['user'] = '';
+            $ta['pass'] = '';
         }
-        if (!empty($ta[port]) and !empty($ta[host])) {
-            $ta[host]=''.$ta[host].':';
-        } elseif (!empty($ta[host])) {
-            $ta[host]=$ta[host];
+        if (!empty($ta['port']) and !empty($ta['host'])) {
+            $ta['host']=''.$ta['host'].':';
+        } elseif (!empty($ta['host'])) {
+            $ta['host']=$ta['host'];
+            $ta['port']='';
         }
-        if (!empty($ta[path])) {
+        if (!empty($ta['path'])) {
             $tu='';
-            $tok=strtok($ta[path], "\\/");
+            $tok=strtok($ta['path'], "\\/");
             while (strlen($tok)) {
                 $tu.=rawurlencode($tok).'/';
                 $tok=strtok("\\/");
             }
-            $ta[path]='/'.trim($tu, '/');
+            $ta['path']='/'.trim($tu, '/');
         }
-        if (!empty($ta[query])) {
-            $ta[query]='?'.$ta[query];
+        if (!empty($ta['query'])) {
+            $ta['query']='?'.$ta['query'];
         }
-        if (!empty($ta[fragment])) {
-            $ta[fragment]='#'.$ta[fragment];
+        if (!empty($ta['fragment'])) {
+            $ta['fragment']='#'.$ta['fragment'];
         }
-        return implode('', array($ta[scheme], $ta[user], $ta[pass], $ta[host], $ta[port], $ta[path], $ta[query], $ta[fragment]));
+        return implode('', array($ta['scheme'], $ta['user'], $ta['pass'], $ta['host'], $ta['port'], $ta['path'], $ta['query'], $ta['fragment']));
     }
 }
